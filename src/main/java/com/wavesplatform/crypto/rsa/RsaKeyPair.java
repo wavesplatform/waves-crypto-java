0a12d6557d4c56a15325139994e6f8a8c6e20324
Maxim Smolyakov <smolyakovmaxim@gmail.com>
2019-10-18 19:24:51 +0300

    rsa separate pair and public key

---
+++ src/main/java/com/wavesplatform/crypto/rsa/RsaKeyPair.java
@@ -0,0 +1,119 @@
+package com.wavesplatform.crypto.rsa;
+
+import com.wavesplatform.crypto.Base58;
+import com.wavesplatform.crypto.Bytes;
+import com.wavesplatform.crypto.HashAlg;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.KeyPair;
+import java.security.*;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class RsaKeyPair {
+
+    public static RsaKeyPair from(byte[] privateKeyBytes) {
+        return new RsaKeyPair(privateKeyBytes);
+    }
+
+    public static RsaKeyPair random() {
+        return new RsaKeyPair();
+    }
+
+    private final BouncyCastleProvider bcp;
+    private final PrivateKey privateKey;
+    private final PublicKey publicKey;
+
+    public RsaKeyPair(byte[] privateKeyBytes) throws IllegalArgumentException {
+        this.bcp = new BouncyCastleProvider();
+        PKCS8EncodedKeySpec ks = new PKCS8EncodedKeySpec(privateKeyBytes);
+        try {
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            this.privateKey = kf.generatePrivate(ks);
+            RSAPrivateCrtKey privateCrtKey = (RSAPrivateCrtKey) this.privateKey;
+            RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(privateCrtKey.getModulus(), privateCrtKey.getPublicExponent());
+            this.publicKey = kf.generatePublic(publicKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public RsaKeyPair() {
+        this.bcp = new BouncyCastleProvider();
+
+        KeyPairGenerator gen;
+        try {
+            gen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new Error(e);
+        }
+        gen.initialize(2048, new SecureRandom());
+
+        KeyPair keys = gen.generateKeyPair();
+        this.privateKey = keys.getPrivate();
+        this.publicKey = keys.getPublic();
+    }
+
+    public byte[] privateKey() {
+        return this.privateKey.getEncoded();
+    }
+
+    public byte[] publicKey() {
+        return this.publicKey.getEncoded();
+    }
+
+    public byte[] sign(HashAlg alg, byte[] source) {
+        try {
+            Signature sig = initJSignature(alg);
+            sig.initSign(this.privateKey);
+            sig.update(source);
+            return sig.sign();
+        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+            throw new Error(e);
+        }
+    }
+
+    public boolean isSignatureValid(HashAlg alg, byte[] message, byte[] proof) {
+        try {
+            Signature sig = initJSignature(alg);
+            sig.initVerify(this.publicKey);
+            sig.update(message);
+            return sig.verify(proof);
+        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+            throw new Error(e);
+        }
+    }
+
+    private Signature initJSignature(HashAlg alg) throws NoSuchAlgorithmException {
+        return Signature.getInstance(alg.value() + "withRSA", bcp);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RsaKeyPair keyPair = (RsaKeyPair) o;
+        return Arrays.equals(this.privateKey(), keyPair.privateKey()) &&
+                Arrays.equals(this.publicKey(), keyPair.publicKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.privateKey(), this.publicKey());
+    }
+
+    @Override
+    public String toString() {
+        return "RsaKeyPair{" +
+                "privateKey=" + Base58.encode(this.privateKey()) +
+                ", publicKey=" + Base58.encode(this.publicKey()) +
+                '}';
+    }
+
+    //TODO encrypt/decrypt + via pubKey
+
+}
---
+++ src/main/java/com/wavesplatform/crypto/rsa/RsaPublicKey.java
@@ -0,0 +1,76 @@
+package com.wavesplatform.crypto.rsa;
+
+import com.wavesplatform.crypto.Base58;
+import com.wavesplatform.crypto.HashAlg;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class RsaPublicKey {
+
+    public static RsaPublicKey from(RsaKeyPair keyPair) {
+        return new RsaPublicKey(keyPair);
+    }
+
+    public static RsaPublicKey from(byte[] publicKeyBytes) {
+        return new RsaPublicKey(publicKeyBytes);
+    }
+
+    private final BouncyCastleProvider bcp;
+    private final PublicKey key;
+
+    public RsaPublicKey(RsaKeyPair keyPair) {
+        this(keyPair.publicKey());
+    }
+
+    public RsaPublicKey(byte[] bytes) throws IllegalArgumentException {
+        this.bcp = new BouncyCastleProvider();
+        X509EncodedKeySpec ks = new X509EncodedKeySpec(bytes);
+        try {
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            this.key = kf.generatePublic(ks);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public byte[] bytes() {
+        return this.key.getEncoded();
+    }
+
+    public boolean isSignatureValid(HashAlg alg, byte[] message, byte[] proof) {
+        try {
+            Signature sig = Signature.getInstance(alg.value() + "withRSA", bcp);
+            sig.initVerify(this.key);
+            sig.update(message);
+            return sig.verify(proof);
+        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+            throw new Error(e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RsaPublicKey publicKey = (RsaPublicKey) o;
+        return Arrays.equals(key.getEncoded(), publicKey.key.getEncoded());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key);
+    }
+
+    @Override
+    public String toString() {
+        return Base58.encode(this.bytes());
+    }
+
+//TODO encrypt via publicKey
+
+}
